f5ebe47fa77bca4d0554f1ecfb24155afc51f64e
ZekeThoreson <ezekiel.thoreson@colorado.edu>
2023-10-17 10:36:49 -0600

    finished ica8

---
+++ wa/wa7/hw5q3a.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+double a;
+double b;
+cin >> a;
+cin >> b;
+double ans1 = a/b;
+double ans2 = a%b;
+
+cout << ans1 << " is a div b" << endl;
+cout << ans2 << " is a mod b" << endl;
+
+}
+
